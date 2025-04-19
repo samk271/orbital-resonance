1144d8eddbce2e5ac7d50fdfaf1f8dfa8ee607b7
Sam Klemic <samk271@vt.edu>
2025-04-19 15:42:07 -0400

    Functional Pitch Shifting

--- src/GUI/AISettings.py
+++ src/GUI/AISettings.py
@@ -115,7 +115,8 @@ class AISettings(CTkFrame):
         self.listbox.grid(row=1,column=1,rowspan=2,pady=20,padx=10)
 
         self.sr = 16000
-        self.signal = range(500)
+        self.signal = [None]
+        self.shifted_signal = None
         self.closest_note = -1
 
         # doesnt crash if dataset is not found
@@ -136,7 +137,7 @@ class AISettings(CTkFrame):
 
         # creates play sound button todo add function
         self.play_button = CTkButton(parent, text="Play Sound",width=100, height=20,  state="disabled", fg_color="gray25")
-        self.play_button.configure(command=lambda: self.play_sound())
+        self.play_button.configure(command=lambda: self.play_sound(self.signal,self.sr))
         self.play_button.grid(row=3,column=2,pady=10)
 
         #Create name input box
@@ -147,17 +148,20 @@ class AISettings(CTkFrame):
 
         # Create pitch dropdowns
         self.pitch_label = CTkLabel(parent, height=10, text="Pitch:")
-        self.pitch_label.grid(row=2, column=4, sticky="n")
+        self.pitch_label.grid(row=3, column=3, sticky="n")
 
         # Note letter dropdown (A to G)
         self.note_letter_var = tk.StringVar(value="C")
-        self.note_letter_menu = CTkOptionMenu(parent, values=["A", "A#", "B", "B#", "C", "C#", "D", "D#", "E", "E#", "F", "F#", "G", "G#"], variable=self.note_letter_var, width=80)
-        self.note_letter_menu.grid(row=2, column=4, padx=(0,20), pady=(20, 0))
+        self.octave_number_var = tk.StringVar(value="4")
+        self.note_letter_menu = CTkOptionMenu(parent, values=["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"], 
+                                              variable=self.note_letter_var, width=80,
+                                              command=self.update_pitch)
+        self.note_letter_menu.grid(row=3, column=3, padx=(0,20), pady=(20, 0))
 
         # Octave number dropdown (2 to 7)
-        self.octave_number_var = tk.StringVar(value="4")
-        self.octave_number_menu = CTkOptionMenu(parent, values=[str(i) for i in range(2, 8)], variable=self.octave_number_var,width=50)
-        self.octave_number_menu.grid(row=2, column=4, padx=(100, 0), pady=(20, 0))
+        self.octave_number_menu = CTkOptionMenu(parent, values=[str(i) for i in range(2, 8)], variable=self.octave_number_var,width=50,
+                                                command=self.update_pitch)
+        self.octave_number_menu.grid(row=3, column=3, padx=(100, 0), pady=(20, 0))
 
         # creates add button todo add function
         self.save_button = CTkButton(parent, text="Save Sample", fg_color="gray25", state="disabled")
@@ -177,6 +181,7 @@ class AISettings(CTkFrame):
         wav_file = self.listbox.get()
         fs, x = wav.read(os.path.join(wav_dir,wav_file))
 
+        self.closest_note = self.find_nearest_midi(x,fs)
         self.sr=fs
         self.signal=x
 
@@ -203,13 +208,23 @@ class AISettings(CTkFrame):
             x=self.signal
 
         self.audio_frame.update_waveform(new_signal=x, new_sample_rate=self.sr)
+
+        left, right = self.audio_frame.get_crop_indices()
+        self.closest_note = self.find_nearest_midi(self.signal[left:right],self.sr)
+
+        #Change displayed pitch to closest midi
+        self.set_pitch_dropdown(note_str=librosa.midi_to_note(self.closest_note))
         
 
 
-    def play_sound(self):
+    def play_sound(self, signal, sr):
 
-        wav.write("./AUDIO/temp_wav.wav",self.sr, self.signal[
-            int(len(self.signal)*self.audio_frame.left_crop):int(len(self.signal)*self.audio_frame.right_crop)])
+        left, right = self.audio_frame.get_crop_indices()
+
+        if (self.shifted_signal is None):
+            wav.write("./AUDIO/temp_wav.wav", sr, signal[left:right])
+        else:
+            wav.write("./AUDIO/temp_wav.wav", sr, self.shifted_signal[left:right])
             
 
         sound = pygame.mixer.Sound("./AUDIO/temp_wav.wav")
@@ -230,7 +245,7 @@ class AISettings(CTkFrame):
         self.save_button.configure(state="normal", fg_color=self.select_button.cget("fg_color"))
 
         
-    #Add planet to solar system
+    #Save sample
     def add_sample_to_list(self):
 
         sample_name = self.sample_name_input.get("1.0",'end-1c')
@@ -244,6 +259,14 @@ class AISettings(CTkFrame):
             'pitch':self.closest_note,
             'midi_array':[[]*4]*3
         }
+
+        left, right = self.audio_frame.get_crop_indices()
+        print(left, right)
+
+        if not (os.path.isdir(f"./AUDIO/user_samples/{sample_name}")):
+            os.mkdir(f"./AUDIO/planets/{sample_name}")
+
+        wav.write(f"./AUDIO/user_samples/{sample_name}/{sample_name}_{self.closest_note}",self.sr, self.signal[left:right])
 
         self.planet_manager.samples[sample_name] = sample_data
 
@@ -267,14 +290,14 @@ class AISettings(CTkFrame):
                     self.generate_button.configure(text = "Generate", state="normal", fg_color=self.select_button.cget("fg_color"))
 
                 self.sr = 16000
-                self.closest_note, self.signal = self.autotune_to_nearest_midi(audio,self.sr)
+                self.closest_note = self.find_nearest_midi(audio,self.sr)
+                self.signal = audio
 
                 self.generate_button.configure(text = "Generate", state="normal", fg_color=self.select_button.cget("fg_color"))
-
-                self.pitch_label.configure(text="Pitch: " + librosa.midi_to_note(self.closest_note))
+                #Change displayed pitch to closest midi
+                self.set_pitch_dropdown(note_str=librosa.midi_to_note(self.closest_note))
 
                 self.update_plot()
-                self.play_sound()
                 self.play_button.configure(state="normal", fg_color=self.select_button.cget("fg_color"))
                 self.sample_name_input.delete('1.0', tk.END)
                 self.sample_name_input.insert(index=tk.END,text =f"sample_{num_user_samples}")
@@ -284,16 +307,26 @@ class AISettings(CTkFrame):
 
         start_pipe()
 
-    def on_close(self):
-        if self.after_id:
-            self.after_cancel(self.after_id)
-        self.destroy()
-
-
-    def autotune_to_nearest_midi(self, y, sr):
+    #Sets the pitch menu to the param note
+    def set_pitch_dropdown(self, note_str):
         """
-        Given an audio signal and sample rate, returns the closest MIDI note
-        and an autotuned version of the signal.
+        Given a string note like 'C#5', sets the pitch dropdowns accordingly.
+
+        Parameters:
+        - note_str: str, musical note in the format like 'C4', 'D#5', etc.
+        """
+
+        octave = note_str[-1]
+        note_letter = note_str[:-1]
+
+        # Update the dropdown values
+        self.note_letter_var.set(note_letter)
+        self.octave_number_var.set(octave)
+
+
+    def find_nearest_midi(self, y, sr):
+        """
+        Given an audio signal and sample rate, returns the closest MIDI note of the signal.
         
         Parameters:
         - y: np.ndarray, the input audio signal.
@@ -318,14 +351,23 @@ class AISettings(CTkFrame):
         # Step 3: Convert to MIDI note
         midi_note = int(np.round(librosa.hz_to_midi(median_f0)))
 
-        # Step 4: Compute pitch shift (in semitones)
-        detected_midi = librosa.hz_to_midi(median_f0)
-        semitone_shift = midi_note - detected_midi
+        return midi_note
+    
+    def update_pitch(self, _=None):
+        """
+        Pitch shifts the signal to the note in the pitch menu, saves to temp
+        """
 
-        # Step 5: Pitch shift the signal to the nearest MIDI note
-        y_tuned = librosa.effects.pitch_shift(y, sr=sr, n_steps=semitone_shift)
+        if (self.signal[0] is None):
+            return
 
-        return midi_note, y_tuned
+        desired_note = self.note_letter_var.get() + self.octave_number_var.get()
+        desired_midi = librosa.note_to_midi(desired_note)
+        steps_to_shift = desired_midi - self.closest_note
+
+        self.shifted_signal = librosa.effects.pitch_shift(self.signal, sr=self.sr, n_steps=steps_to_shift)
+
+        self.play_sound(signal=self.shifted_signal,sr=self.sr)
 
 
 # Redirect tqdm output to a CTkLabel  todo there is a CTkProgressBar that might look nicer
